6434264acd3238ff8b0779324b4c02273cd95680
lisheng <499817366@qq.com>
2019-11-18 08:38:23 +0000

    update test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const selenium = require('selenium-webdriver')
+const { until, By } = selenium
 const ie = require('selenium-webdriver/ie')
 const capabilities = require('selenium-webdriver/lib/capabilities')
 require('iedriver')
@@ -7,8 +8,19 @@ var ieOption = (new ie.Options()).requireWindowFocus(true)
 const Builder = selenium.Builder
 
 var builder = new Builder().forBrowser(capabilities.Browser.IE).setIeOptions(ieOption)
-
-builder.build().then(async (driver) => {
+var driver
+builder.build().then(async (d) => {
+    driver = d
+    process.on('exit', () => {
+        driver.close()
+    })
     await driver.get("https://www.baidu.com")
-    throw "abcde"
+    var input = await driver.wait(until.elementLocated(By.id('kw')))
+    await input.sendKeys("test")
+    var search = await driver.wait(until.elementLocated(By.id("su")))
+    await search.click()
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    await driver.wait(until.elementLocated(By.xpath("//a[contains(text(),'百度翻译')]")))
+}).finally(() => {
+    driver.close()
 })
